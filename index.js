b5b3567115a10cc63701dd634845cfeebe106b7e
Jacob Harvey <jacobharv00@gmail.com>
2021-01-27 01:04:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,9 @@ function destructivelyRemoveLastKitten(name) {
   kittens.pop(name)
   return kittens
   }
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+  return kittens
+  }
+  
